dbe59fd2846ead6e9bed71c00cf100ba52087ffc
Niann <348258165@qq.com>
2017-09-29 15:23:13 +1000

    linear model

--- part_2/contest/rlTrain.py
+++ part_2/contest/rlTrain.py
@@ -1,7 +1,26 @@
+import sys
+import util
+
+alpha = 0.1 # learning rate
+
 def extractFeatures(state, action, nextState, agent):
     # return features of state
     pass
 
-def train(features, actions, labels):
+def train(features, actions, labels, model):
     # return trained weights
-    pass
+    if model == "Linear":
+        return trainLinear(features, labels)
+    else:
+        print "Undefined model"
+        sys.exit(1)
+
+def trainLinear(features, labels):
+    # initialize weights
+    weights = util.Counter()
+    # stochastic gradient descent
+    for i in len(labels):
+        diff = labels[i] - features[i] * weights
+        for feature, value in features[i].iteritems():
+            weights[feature] += alpha * diff * value
+    return weights
