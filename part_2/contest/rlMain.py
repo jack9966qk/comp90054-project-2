e52a647c58830b0cdbfbe4cd056d8552b6f517fc
Jack <Jack@Jacks-MacBook-Pro.local>
2017-10-05 12:23:38 +1100

    clean

--- part_2/contest/rlMain.py
+++ part_2/contest/rlMain.py
@@ -1,30 +1,16 @@
-dir = "teams/"
-t1 = "baselineTeam"
-t2 = "modSelection"
-dirt1 = dir+t1+'/'
-dirt2 = dir+t2+'/'
-
 import os
 import sys
-teamName = t2
-dir = "teams/{}/".format(teamName)
-sys.path.append(dir)
-
-
 import pickle
 from subprocess import call
 import os
 import cPickle
 import time
 import textDisplay
-import reward
 from capture import CaptureRules
-from rlTrain import extractFeatures, train
+# from rlTrain import extractFeatures, train
 import imp
 from multiprocessing import Pool
 from functools import partial
-import IOutil
-#from Team1 import Team1
 
 WEIGHT_FILENAME = "rlWeights.json"
 
@@ -36,28 +22,12 @@ def recoverFromReplay(replay):
     rules = CaptureRules()
     game = rules.newGame(replay["layout"], replay["agents"], display, replay["length"], False, False)
     state = game.state
-    # print("AGENT POSITION")
-    # print(state.getAgentPosition(0))
-    # print(state.getAgentPosition(1))
-    # print(state.getAgentPosition(2))
-    # print(state.getAgentPosition(3))
     
     for idx, action in replay["actions"]:
         agent = replay["agents"][idx]
 
         observation = state.makeObservation(agent.index)
-        # print(idx)
-        # print("AGENT POSITION")
         sequence.append((observation, action, agent))
-        # print(observation.getAgentPosition(0))
-        # print(observation.getAgentPosition(1))
-        # print(observation.getAgentPosition(2))
-        # print(observation.getAgentPosition(3))
-        # print("AGENT DISTANCES")
-        # print(observation.getAgentDistances())
-        # state = state.makeObservation(agent.index)
-        
-
         state = state.generateSuccessor(idx, action)
         rules.process(state, game)
     # final state for each agent
@@ -81,7 +51,7 @@ def recoverFromReplay(replay):
 def runGames(redTeam, blueTeam, dir, numGames, prefix):
     call(["python", "./capture.py",
           "-r", redTeam, "-b", blueTeam, "-l", "RANDOM",
-          "-q", "-n", str(numGames), "--record={}/{}replay".format(dir, prefix)])
+          "-Q", "-n", str(numGames), "--record={}/{}replay".format(dir, prefix)])
     print("{} finished".format(prefix))
 
 def simulateGames(redTeam, blueTeam, numRuns=1, numGamesPerRun=1, numProcesses=20):
@@ -144,28 +114,16 @@ def addFeaturesOneGame(sequence):
     return zip(states, actions, agents, features, mods)
 
 if __name__ == "__main__":
-    imp.load_source("player0", dirt1+"myTeam.py")
-    imp.load_source("player1", dirt2+"myTeam.py")
+    imp.load_source("player0", "baselineTeam.py")
+    imp.load_source("player1", "baselineTeam.py")
     
-    #dir = simulateGames(dirt2+"myTeam", dirt1+"myTeam", numGamesPerRun=1, numRuns=10)
+    dir = simulateGames("baselineTeam", "baselineTeam", numGamesPerRun=50, numRuns=500)
     # all games finished, load data from replay files
-    #dir = "replay/Oct-03-14-00-15" #100
-    dir = "replay/Oct-04-18-12-14" #10
-    #dir = "replay/Oct-04-18-08-42" #1
-    replayData = loadReplayFiles(dir)
-    replayDataWithFeat = addFeatures(replayData)
-    instances = addLabels(replayDataWithFeat)
-    features, actions, labels = makeTrainingSet(instances)
-    
-    IOutil.saveFile("tfeatures.json",features)
-    IOutil.saveFile("tactions.json",actions)
-    IOutil.saveFile("tlabels.json",labels)
-    
-    #features = IOutil.loadFile("tfeatures.json")
-    #actions = IOutil.loadFile("tactions.json")
-    #labels = IOutil.loadFile("tlabels.json") 
-    #weight = train(features, actions, labels)
-    weight = train(features, actions, labels,model ="MLP")
+    # replayData = loadReplayFiles(dir)
+    # replayDataWithFeat = addFeatures(replayData)
+    # instances = addLabels(replayDataWithFeat)
+    # features, actions, labels = makeTrainingSet(instances)
+    # weight = train(features, actions, labels,model ="MLP")
 
 # # save weight
   #  IOutil.saveFile(WEIGHT_FILENAME,weight)
